c9355a4bc2122d96a31b4e9278b825dc0edbcbe8
Nicolas Andres Contreras Orellana <ncontreras@internacional.cl>
2025-12-13 19:22:12 -0300

    feat: provide AuthGuard in AuthModule and correct its AuthService import path.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '../../shared/guards/auth.guard';
 
 @Module({
-  providers: [AuthService],
-  exports: [AuthService],
+  imports: [ConfigModule],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthService, AuthGuard],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/shared/guards/auth.guard.ts
+++ src/shared/guards/auth.guard.ts
@@ -4,11 +4,11 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../../modules/auth/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
